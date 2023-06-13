be4a4171fb90db52bf1928161d91ed9f1fe398fb
Saul Vaquero Garvin <saulvg@MacBook-Air-de-Saul.local>
2023-06-13 08:04:32 +0200

    layout + estructura carpetas + routing

--- web-pokemon/src/App.js
+++ web-pokemon/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Header} from './layout/'
 import { HomePage, Contact, NotFound, About } from './pages/';
+import {BrowserRouter, Routes, Route, } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/not-found' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ web-pokemon/src/pages/index.js
@@ -0,0 +1,12 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import HomePage from "./HomePage/HomePage";
+import NotFound from "./NotFound/NotFound";
+
+
+export {
+    About,
+    Contact,
+    HomePage,
+    NotFound
+}
