e6fa6660016631055cde3bd3c56223f3a081c5ec
Antonio Jimenez <antoniojimenezflores90@gmail.com>
2023-06-16 17:51:28 +0200

    formulario contacto más ok google map

--- web-pokemon/src/pages/Contact/Contact.jsx
+++ web-pokemon/src/pages/Contact/Contact.jsx
@@ -1,19 +1,77 @@
-
+import "./contact.css";
 import { Map } from "../../compartidos";
+import { useState } from "react";
 
 
 export default function Contact() {
+
+    const [formContact, setFormContact] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        setFormContact({
+            ...formContact,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formContact);
+
+        setFormContact({
+            name: '',
+            email: '',
+            message: ''
+        });
+    };
+
     return (
         <>
-            <section>
-                <header>
-                    <h2>Contacto</h2>
-                </header>
-                <p>Formulario</p>
-            </section>
-            <section>
-                <Map />
-            </section>
+            <div >
+                <section>
+
+                    <div className="contact-container">
+                        <h2>Contacto</h2>
+                        <form onSubmit={handleSubmit} className="contact-form">
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Nombre"
+                                value={formContact.name}
+                                onChange={handleChange}
+                                required
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                value={formContact.email}
+                                onChange={handleChange}
+                                required
+                            />
+                            <textarea
+                                name="message"
+                                placeholder="Mensaje"
+                                value={formContact.message}
+                                onChange={handleChange}
+                                required
+                            ></textarea>
+                            <button type="submit">Enviar</button>
+                        </form>
+                    </div>
+
+                </section>
+
+                <section>
+                    <p>GoogleMap</p>
+                    <Map />
+                </section>
+            </div>
         </>
 
     );
