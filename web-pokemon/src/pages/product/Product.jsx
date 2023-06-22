e52fe2d1ff1ab733831d4028cc9f5e5a27404174
Antonio Jimenez <antoniojimenezflores90@gmail.com>
2023-06-22 13:57:26 +0200

    redireccion no producto encontrado a not-found

--- web-pokemon/src/pages/product/Product.jsx
+++ web-pokemon/src/pages/product/Product.jsx
@@ -1,10 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { ErrorMessage } from "../../compartidos";
 
 export default function Product() {
     let { id } = useParams();
     const [filteredProduct, setFilteredProduct] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         getProduct(id);
@@ -16,26 +16,25 @@ export default function Product() {
         getProduct.length > 0 ? setFilteredProduct(getProduct[0]) : setFilteredProduct({});
     };
 
+    if (!filteredProduct.id) {
+        navigate('/not-found');
+    }
+
     return (
         <>
             <section>
-                {
-                    !filteredProduct.length ? (
-                        <article>
-                            <header>
-                                <h2>{filteredProduct.name}</h2>
-                            </header>
-                            <div>
-                                <img src={filteredProduct.image} alt={filteredProduct.name} />
-                            </div>
-                            <p>{filteredProduct.description}</p>
-                            <p>{filteredProduct.price}</p>
 
-                        </article>
-                    ) : (
-                        <ErrorMessage message={"Producto no encontrado"} />
-                    )
-                }
+                <article>
+                    <header>
+                        <h2>{filteredProduct.name}</h2>
+                    </header>
+                    <div>
+                        <img src={filteredProduct.image} alt={filteredProduct.name} />
+                    </div>
+                    <p>{filteredProduct.description}</p>
+                    <p>{filteredProduct.price}</p>
+
+                </article>
 
             </section>
         </>
